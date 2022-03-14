0eddadb891abc7a8aa839005d0b09cbb1c820418
Ahmed ZEKRI <az@vyzyo.com>
2022-03-14 16:58:04 +0100

    Starting animation

--- app/src/main/java/com/example/socialmediaapp/presentation/utils/Navigation.kt
+++ app/src/main/java/com/example/socialmediaapp/presentation/utils/Navigation.kt
@@ -4,7 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.socialmediaapp.presentation.SplashScreen
+import com.example.socialmediaapp.splash.SplashScreen
 
 @Composable
 fun Navigation() {
--- app/src/main/java/com/example/socialmediaapp/splash/Splash.kt
+++ app/src/main/java/com/example/socialmediaapp/splash/Splash.kt
@@ -1,9 +1,11 @@
-package com.example.socialmediaapp.presentation
+package com.example.socialmediaapp.splash
+
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -15,6 +17,10 @@ import com.example.socialmediaapp.common.LOGO_SPLASH_DESCRIPTION
 @Composable
 fun SplashScreen(navController: NavController) {
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        val scale = remember {
+            androidx.compose.animation.core.Animatable(0f)
+        }
+
 
         Image(
             painter = painterResource(id = R.drawable.ic_launcher_background),
